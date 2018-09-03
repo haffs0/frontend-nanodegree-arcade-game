a98e71c53019ea7f04679b64f0b702c082629842
haffs0 <abuafeezabolarinwa@yahoo.com>
2018-09-03 15:17:16 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // Enemies our player must avoid
-var Enemy = function(x, y) {
+'use strict'
+let Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -34,43 +35,42 @@ Enemy.prototype.render = function() {
 
 //enemy update
 function updateGameArea() {
-    enemy1.update(1);    
-    enemy2.update(1.5);
-    enemy3.update(1.8);
-    enemy4.update(1.2);    
-    enemy5.update(1.5);
-    enemy6.update(0.5);      
+    allEnemies.forEach(function(enemy) {
+        enemy.update(Math.random() * 4)
+    });  
 }
   
 // Now write your own player class
-var Player = function(x, y) {
+let Player = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.x = x;
-    this.y = y;
+    this.x = 200;
+    this.y = 390;
     this.sprite = 'images/char-boy.png';
    //this.interval = setInterval(playerUpdate, 1000);
 };
 
 // This class requires an update(), render() and
 // a handleInput() method.
+//Reset the player position
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 390;
+}
+
 //update section of player and enemies when they collide
 Player.prototype.update = function() {
-    var position = this;
+    const position = this;
     allEnemies.forEach(function(enemy) {
-        var xMeet = Math.abs(position.x - enemy.x);
-        var yMeet = Math.abs(position.y - enemy.y);
-        console.log(xMeet);
-        console.log(yMeet);
-        if (xMeet < 60 && yMeet < 60) {
-            position.x = 200;
-            position.y = 390; 
+        let xMeet = Math.abs(position.x - Math.floor(enemy.x));
+        let yMeet = Math.abs(position.y - enemy.y);
+        if (xMeet < 70 && yMeet < 70) {
+            position.reset();
         }
-});
-
+    });
 };
 // Draw the player on the screen, required method for game
 Player.prototype.render = function() {
@@ -101,17 +101,15 @@ Player.prototype.handleInput = function(ev){
 
     //if the player reached the water
     if (this.y === 0) {
-        this.x = 200;
-        this.y = 390;
+        this.reset();
         alert("you win");
     }
-
 };
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-     var allowedKeys = {
+     const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
@@ -121,14 +119,14 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
-// Now instantiate your objects.
-let enemy1 = new Enemy(0, 60);
-let enemy2 = new Enemy(0, 145);
-let enemy3 = new Enemy(0, 225);
-let enemy4 = new Enemy(0, 60);
-let enemy5 = new Enemy(200, 145);
-let enemy6 = new Enemy(200, 225);
 // Place all enemy objects in an array called allEnemies
-let allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
+let allEnemies = [
+    new Enemy(0, 60),
+    new Enemy(0, 145),
+    new Enemy(0, 225),
+    new Enemy(0, 60),
+    new Enemy(200, 145),
+    new Enemy(200, 225)
+];
 // Place the player object in a variable called player
-let player = new Player(200, 390);
+let player = new Player();
